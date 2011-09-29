6f6a3ae17a6605fbb87f22efc5b250964300b88e
Dallas Read <dallas@excitecreative.ca>
2011-09-29 12:03:06 -0300

    viewport

--- post.php
+++ post.php
@@ -20,7 +20,7 @@
 <html>
 <head>
 	<title><? echo $title; ?> :: DallasRead.com</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, user-scalable=no">
 	<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 </head>
 
