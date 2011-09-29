bda70567d6b544915f2acd1a53175154a5960e09
Dallas Read <dallas@excitecreative.ca>
2011-09-29 12:06:59 -0300

    Viewport Test

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <html>
 <head>
 	<title>DallasRead.com</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 </head>
 
--- post.php
+++ post.php
@@ -20,6 +20,7 @@
 <html>
 <head>
 	<title><? echo $title; ?> :: DallasRead.com</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 </head>
 
